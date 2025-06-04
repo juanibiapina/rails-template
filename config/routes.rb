a0a5cf89b6f5e088051adb3a83ecc13529bd5382
Juan Ibiapina <juanibiapina@gmail.com>
2025-06-04 09:40:13 +0200

    feat: Configure devise for User

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  # Authentication
+  devise_for :users
+
+  # Home page
+  get "home/index"
+
   # Defines the root path route ("/")
   root "home#index"
 end
